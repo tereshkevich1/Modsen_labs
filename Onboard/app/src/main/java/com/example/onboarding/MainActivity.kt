f2f4fd8b88172c0cdd3fa5e1aa654398b58ef4b3
tereshkevich1 <tereshkevich.cergey@mail.ru>
2024-06-18 17:25:49 +0300

    The first 4 screens are done

--- Onboard/app/src/main/java/com/example/onboarding/MainActivity.kt
+++ Onboard/app/src/main/java/com/example/onboarding/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.onboarding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.onboarding.main.OnBoardScreen
 import com.example.onboarding.ui.theme.OnboardingTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +12,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             OnboardingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                OnBoardScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OnboardingTheme {
-        Greeting("Android")
     }
 }
